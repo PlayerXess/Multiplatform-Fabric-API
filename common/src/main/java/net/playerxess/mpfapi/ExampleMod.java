d9589aa24082fe2ccb7ad37f504ffbb075f509c2
PlayerXess <enalethan368@gmail.com>
2025-03-03 08:09:15 -0500

    Actual Initial Commit (No Migrated Mappings; README Not Finished)

---
+++ common/src/main/java/net/playerxess/mpfapi/ExampleMod.java
@@ -0,0 +1,9 @@
+package net.playerxess.mpfapi;
+
+public final class ExampleMod {
+    public static final String MOD_ID = "mpfapi";
+
+    public static void init() {
+        // Write common init code here.
+    }
+}
---
+++ fabric-like/src/main/java/net/playerxess/mpfapi/fabriclike/ExampleModFabricLike.java
@@ -0,0 +1,10 @@
+package net.playerxess.mpfapi.fabriclike;
+
+import net.playerxess.mpfapi.ExampleMod;
+
+public final class ExampleModFabricLike {
+    public static void init() {
+        // Run our common setup.
+        ExampleMod.init();
+    }
+}
