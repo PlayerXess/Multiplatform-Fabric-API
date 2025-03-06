063c19bd7fcd69c5011e995d784188cb0c21cdbd
PlayerXess <enalethan368@gmail.com>
2025-03-05 20:38:01 -0500

    Mappings Merged. Working on remappers and sat4j

--- common/src/main/java/net/playerxess/mpfapi/MPFAPI.java
+++
@@ -1,9 +0,0 @@
-package net.playerxess.mpfapi;
-
-public final class MPFAPI {
-    public static final String MOD_ID = "mpfapi";
-
-    public static void init() {
-        // Write common init code here.
-    }
-}
